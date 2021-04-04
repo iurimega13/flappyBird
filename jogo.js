1c468345556cc5a1559bf97dc858c1c15fd4924b
iurimega13 <iurimega13@gmail.com>
2021-04-03 22:54:34 -0300

    Desenhando o bird na tela

--- jogo.js
+++ jogo.js
@@ -1,8 +1,21 @@
-console.log('[DevSoutinho] Flappy Bird');
-console.log('Inscreva-se no canal :D https://www.youtube.com/channel/UCzR2u5RWXWjUh7CwLSvbitA');
+console.log('Flappy Bird');
 
 const sprites = new Image();
 sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
+
+function loop() {
+    contexto.drawImage(
+        sprites,
+        0, 0, // Localização do sprite dentro do arquivo
+        34.24, 24,// Tamanho do recorte na sprite
+        0, 50, // Local onde vai  ser dezenhado o sprite
+        34.24, 24,// Tamanho que vai ser dezenhado no canvas 
+    );
+    
+    requestAnimationFrame(loop); 
+}
+
+loop();
